7791fb463566789c7a01ec0bae7d41756e4a01b5
Danilo de Oliveira <danilo.oliveira@grupoboticario.com.br>
2023-05-20 16:50:36 -0300

    Breadcrumb ajustado

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,16 +1,22 @@
 'use client';
 
-import {
-  NavigateNext,
-} from '@mui/icons-material';
+import { NavigateNext } from '@mui/icons-material';
 import {
   Box,
   Breadcrumbs as MuiBreadcrumbs,
   Link,
   Typography,
 } from '@mui/material';
+import { usePathname } from 'next/navigation';
 
 export default function Breadcrumbs() {
+  const pathname = usePathname();
+  const paths = [
+    { label: 'Home', link: '/', pattern: /^\//g },
+    { label: 'Buscar médicos', link: '/booking', pattern: /^\/booking/g },
+    { label: 'Agendamento', link: '/booking', pattern: /^\/booking\/\d.*/g },
+  ].filter((p) => p.pattern.test(pathname));
+
   return (
     <MuiBreadcrumbs
       component={Box}
@@ -18,18 +24,23 @@ export default function Breadcrumbs() {
       aria-label="breadcrumb"
       my={3}
     >
-      <Link
-        underline="hover"
-        key="2"
-        color="inherit"
-        href="/"
-        onClick={() => {}}
-      >
-        Home
-      </Link>
-      <Typography key="3" color="text.primary">
-        Agendamento de Consulta
-      </Typography>
+      {paths.map((b, index) =>
+        index < paths.length - 1 ? (
+          <Link
+            underline="hover"
+            key="2"
+            color="inherit"
+            href={b.link}
+            onClick={() => {}}
+          >
+            {b.label}
+          </Link>
+        ) : (
+          <Typography key="3" color="text.primary">
+            {b.label}
+          </Typography>
+        )
+      )}
     </MuiBreadcrumbs>
   );
 }
